8476ea602ddf4f918e63f9667e83551511e63fda
Fernando Machado <fernandomachadoweb@gmail.com>
2022-09-13 21:26:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // coloque a nota para teste
+        int nota = 70;
+
+        // se nota maior ou igual a 70, então aprovado
+        // se nota for menor que 70, então reprovado
+        // função if-else
+
+        if (nota >= 70) {
+            System.out.println("Aluno Aprovado!");
+        } else {
+            System.out.println("Aluno Reprovado!");
+        }
+
+
+    }
+}
